0bb703357608ecb2dc6d0d5fb3bb837c3f691446
mstilyan <stilyan.mladenov@gmail.com>
2019-12-15 22:11:16 +0200

    Updates

---
+++ precache-manifest.41dab7e5d9daedca2456116ccf33d8ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d81d9c97b1fbc875645d2642818ad1d4",
+    "url": "/airbnb-stats-web/static/media/map_marker.d81d9c97.png"
+  },
+  {
+    "revision": "712e65a49b8ae538cefb",
+    "url": "/airbnb-stats-web/static/js/runtime~main.a62400bf.js"
+  },
+  {
+    "revision": "6fccf7d4a0599187d99d",
+    "url": "/airbnb-stats-web/static/js/main.f1bb053d.chunk.js"
+  },
+  {
+    "revision": "92443966ea3f66e1d47c",
+    "url": "/airbnb-stats-web/static/js/2.ff8f0719.chunk.js"
+  },
+  {
+    "revision": "6fccf7d4a0599187d99d",
+    "url": "/airbnb-stats-web/static/css/main.d2678423.chunk.css"
+  },
+  {
+    "revision": "d43bea5b15912415b1103ffbb3baceea",
+    "url": "/airbnb-stats-web/index.html"
+  }
+];
